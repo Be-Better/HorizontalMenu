f030691c14f194e7e261ae9a23980939c67f53d1
chen <chen@macbook-air.local>
2016-01-09 12:04:27 +0800

    Initial Commit

---
+++ HorizontalMenuDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HorizontalMenuDemo
+//
+//  Created by chen on 16/1/9.
+//  Copyright © 2016年 chen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HorizontalMenuDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HorizontalMenuDemo
+//
+//  Created by chen on 16/1/9.
+//  Copyright © 2016年 chen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
